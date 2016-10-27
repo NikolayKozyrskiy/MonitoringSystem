76dbee4828172a57e1a7fc589ca96ce84ef9123d
KrasnikovRoman <roman.krasnikov.96@mail.ru>
2016-10-28 01:43:57 +0300

    Create Frame class

---
+++ src/main/Java/Frame.java
@@ -0,0 +1,101 @@
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by roman on 27.10.16.
+ */
+public class Frame {
+    private int frameNumber;
+    private String type;
+    private double deltaTime;
+    private int frameLength;
+    private double timeRelative;
+    private String protocol;
+    private Date frameTime;
+    private Date frameDate;
+    private Calendar receivingTime;
+
+    public Frame(int frameNumber, String type, double deltaTime, int frameLength, double timeRelative, String protocol, Date frameTime, Date frameDate) {
+        this.frameNumber = frameNumber;
+        this.type = type;
+        this.deltaTime = deltaTime;
+        this.frameLength = frameLength;
+        this.timeRelative = timeRelative;
+        this.protocol = protocol;
+        this.frameTime = frameTime;
+        this.frameDate = frameDate;
+    }
+
+    public int getFrameNumber() {
+        return frameNumber;
+    }
+
+    public void setFrameNumber(int frameNumber) {
+        this.frameNumber = frameNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getDeltaTime() {
+        return deltaTime;
+    }
+
+    public void setDeltaTime(double deltaTime) {
+        this.deltaTime = deltaTime;
+    }
+
+    public int getFrameLength() {
+        return frameLength;
+    }
+
+    public void setFrameLength(int frameLength) {
+        this.frameLength = frameLength;
+    }
+
+    public double getTimeRelative() {
+        return timeRelative;
+    }
+
+    public void setTimeRelative(double timeRelative) {
+        this.timeRelative = timeRelative;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public Date getFrameTime() {
+        return frameTime;
+    }
+
+    public void setFrameTime(Date frameTime) {
+        this.frameTime = frameTime;
+    }
+
+    public Date getFrameDate() {
+        return frameDate;
+    }
+
+    public void setFrameDate(Date frameDate) {
+        this.frameDate = frameDate;
+    }
+
+    public Calendar getReceivingTime() {
+        return receivingTime;
+    }
+
+    public void setReceivingTime(Calendar receivingTime) {
+        this.receivingTime = receivingTime;
+    }
+}
+
